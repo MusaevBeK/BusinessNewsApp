4e75a20de8b55603a955738f5219fa68079de09f
Мусаев94 <beksultan1923@gmail.com>
2022-05-15 01:38:11 +0600

    Initial Commit

---
+++ SomeAppForGitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SomeAppForGitHub
+//
+//  Created by user on 15/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
